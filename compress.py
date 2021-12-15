08982ec57b9905dee3428e5865f5d468e1311c9b
dyf <david.feng@alleninstitute.org>
2021-12-15 11:17:00 -0800

    reporting write time

--- compress.py
+++ compress.py
@@ -25,7 +25,9 @@ def run(num_tiles, resolution, random_seed, input_file, output_file):
         random.seed(random_seed)
 
     total_bytes_read = 0
-    total_time = 0
+    total_read_time = 0
+    total_compress_time = 0
+    total_write_time = 0
     total_bytes_stored = 0
 
     compressor = numcodecs.Blosc(cname='zstd', clevel=1, shuffle=numcodecs.Blosc.SHUFFLE)
@@ -33,31 +35,47 @@ def run(num_tiles, resolution, random_seed, input_file, output_file):
     with h5py.File(input_file) as f:
         ds = f["t00000"]
 
-
-
         for ti in range(num_tiles):
             rslice = random.choice(list(ds.keys()))
 
             print(f"loading {rslice}")
+
+            start = timer()
             data = ds[rslice][resolution]["cells"][()]
-            print(f"loaded {data.shape} {data.nbytes} bytes")
+            end = timer()
+            read_dur = end - start
+            print(f"loaded {data.shape} {data.nbytes} bytes, time {read_dur}s")
 
 
             start = timer()
             za = zarr.array(data, chunks=True, compressor=compressor)
-            print(za.info)
             end = timer()
-            dur = end - start
+            compress_dur = end - start
 
+            print(f"compression time = {compress_dur}, bps = {data.nbytes / compress_dur}, ratio = {za.nbytes/za.nbytes_stored}")
+
+            start = timer()
+
+            start = timer()
+            zarr.save(output_file, za)
+            end = timer()
+            write_dur = end - start
+
+            print(f"write time = {write_dur}s")
             
-
-            print(f"compression time = {dur}, bps = {data.nbytes / dur}, ratio = {za.nbytes/za.nbytes_stored}")
+            
+            total_read_time += read_dur
+            total_compress_time += compress_dur
+            total_write_time += write_dur
 
             total_bytes_read += data.nbytes
             total_bytes_stored += za.nbytes_stored
-            total_time += dur
             
-        print(f"bps {total_bytes_read / total_time}, ratio = {total_bytes_read/total_bytes_stored}")
+        print(f"compress bps {total_bytes_read / total_compress_time}")
+        print(f"compression ratio = {total_bytes_read/total_bytes_stored}")
+        print(f"read bps {total_bytes_read / total_read_time}")
+        print(f"write bps {total_bytes_stored / total_write_time}")
+        
     
 
 if __name__ == "__main__": main()
