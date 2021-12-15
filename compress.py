eee325fcdcfcf82264e48f8dbccc67b25d01304c
dyf <david.feng@alleninstitute.org>
2021-12-15 13:31:54 -0800

    blosc compressor lib

--- compress.py
+++ compress.py
@@ -5,8 +5,20 @@ import numcodecs
 import argparse
 import sys
 import pandas as pd
+import itertools
 
 from timeit import default_timer as timer
+
+def compressor_lib():    
+    cnames = [ 'zstd', 'blosclz', 'lz4', 'lz4hc', 'zlib' ]#, 'snappy' ]
+    shuffles = [ numcodecs.Blosc.SHUFFLE, numcodecs.Blosc.NOSHUFFLE ]
+    clevels = [ 0,1,2,5,9 ]
+    opts = itertools.product(cnames, clevels, shuffles)
+
+    return {
+        f'blosc cname={cname} clevel={clevel} shuffle={shuffle}':numcodecs.Blosc(cname=cname, clevel=clevel, shuffle=shuffle) for cname, clevel, shuffle in opts
+    }
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -34,58 +46,64 @@ def run(num_tiles, resolution, random_seed, input_file, output_data_file, output
 
     all_metrics = []
 
-    compressor = numcodecs.Blosc(cname='zstd', clevel=1, shuffle=numcodecs.Blosc.SHUFFLE)
+    compressors = compressor_lib()
 
     with h5py.File(input_file) as f:
         ds = f["t00000"]
 
         for ti in range(num_tiles):
-            tile_metrics = {}
-
             rslice = random.choice(list(ds.keys()))
-            tile_metrics['tile'] = rslice
-
-            print(f"loading {rslice}")
-
-            start = timer()
-            data = ds[rslice][resolution]["cells"][()]
-            end = timer()
-            read_dur = end - start
-            tile_metrics['read_time'] = read_dur
-            tile_metrics['nbytes'] = data.nbytes
-            tile_metrics['shape'] = data.shape
-            tile_metrics['read_bps'] = data.nbytes / read_dur
-            print(f"loaded {data.shape} {data.nbytes} bytes, time {read_dur}s")
-
-
-            start = timer()
-            za = zarr.array(data, chunks=True, compressor=compressor)
-            end = timer()
-            compress_dur = end - start
-            tile_metrics['compress_time'] = compress_dur
-            tile_metrics['nbytes_written'] = za.nbytes_stored
-            tile_metrics['compress_bps'] = data.nbytes / compress_dur
-            tile_metrics['storage_ratio'] = data.nbytes / za.nbytes_stored
-            print(f"compression time = {compress_dur}, bps = {data.nbytes / compress_dur}, ratio = {za.nbytes/za.nbytes_stored}")
-
-            start = timer()
-            zarr.save(output_data_file, za)
-            end = timer()
-            write_dur = end - start
-            tile_metrics['write_time'] = write_dur
-            tile_metrics['write_bps'] = za.nbytes_stored / write_dur
-            print(f"write time = {write_dur}s")
             
-            total_read_time += read_dur
-            total_compress_time += compress_dur
-            total_write_time += write_dur
+            for cname, compressor in compressors.items():
 
-            total_bytes_read += data.nbytes
-            total_bytes_stored += za.nbytes_stored
+                tile_metrics = {
+                    'compressor': cname,
+                    'tile': rslice
+                }
 
-            all_metrics.append(tile_metrics)
+                print(f"compressor: {cname}")
 
-        df = pd.DataFrame.from_records(all_metrics, index='tile')
+                print(f"loading {rslice}")
+
+                start = timer()
+                data = ds[rslice][resolution]["cells"][()]
+                end = timer()
+                read_dur = end - start
+                tile_metrics['read_time'] = read_dur
+                tile_metrics['nbytes'] = data.nbytes
+                tile_metrics['shape'] = data.shape
+                tile_metrics['read_bps'] = data.nbytes / read_dur
+                print(f"loaded {data.shape} {data.nbytes} bytes, time {read_dur}s")
+
+
+                start = timer()
+                za = zarr.array(data, chunks=True, compressor=compressor)
+                end = timer()
+                compress_dur = end - start
+                tile_metrics['compress_time'] = compress_dur
+                tile_metrics['nbytes_written'] = za.nbytes_stored
+                tile_metrics['compress_bps'] = data.nbytes / compress_dur
+                tile_metrics['storage_ratio'] = data.nbytes / za.nbytes_stored
+                print(f"compression time = {compress_dur}, bps = {data.nbytes / compress_dur}, ratio = {za.nbytes/za.nbytes_stored}")
+
+                start = timer()
+                zarr.save(output_data_file, za)
+                end = timer()
+                write_dur = end - start
+                tile_metrics['write_time'] = write_dur
+                tile_metrics['write_bps'] = za.nbytes_stored / write_dur
+                print(f"write time = {write_dur}s")
+
+                total_read_time += read_dur
+                total_compress_time += compress_dur
+                total_write_time += write_dur
+
+                total_bytes_read += data.nbytes
+                total_bytes_stored += za.nbytes_stored
+
+                all_metrics.append(tile_metrics)
+
+        df = pd.DataFrame.from_records(all_metrics)
         df.to_csv(output_metrics_file)
             
         print(f"compress bps {total_bytes_read / total_compress_time}")
