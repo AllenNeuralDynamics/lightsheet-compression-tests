7c958bfa69f0b40abf708011cbf961bcb106b554
dyf <david.feng@alleninstitute.org>
2021-12-17 10:03:13 -0800

    moving inner loop to a function, zarr consuming a lot of memory

--- compress.py
+++ compress.py
@@ -53,16 +53,42 @@ def main():
         output_data_file=args.output_data_file, 
         output_metrics_file=args.output_metrics_file)
 
+def read_compress_write(dataset, key, compressor, filters, output_data_file):
+    logging.info(f"loading {key}")
+    
+    start = timer()
+    data = dataset[key][()]
+    end = timer()
+    read_dur = end - start
+    logging.info(f"loaded {data.shape}, {data.nbytes} bytes, time {read_dur}s")
+
+    start = timer()
+    za = zarr.array(data, chunks=True, filters=filters, compressor=compressor)
+    end = timer()
+    compress_dur = end - start
+    logging.info(f"compression time = {compress_dur}, bps = {data.nbytes / compress_dur}, ratio = {za.nbytes/za.nbytes_stored}")
+
+    start = timer()
+    zarr.save(output_data_file, za)
+    end = timer()
+    write_dur = end - start
+    logging.info(f"write time = {write_dur}s")
+
+    return {
+        'read_time': read_dur,
+        'bytes_read': za.nbytes,
+        'compress_time': compress_dur,
+        'bytes_written': za.nbytes_stored,
+        'write_time': write_dur,
+        'shape': data.shape
+    }
+
+
 def run(num_tiles, resolution, random_seed, input_file, output_data_file, output_metrics_file):
 
     if random_seed is not None:
         random.seed(random_seed)
 
-    total_bytes_read = 0
-    total_read_time = 0
-    total_compress_time = 0
-    total_write_time = 0
-    total_bytes_stored = 0
 
     all_metrics = []
 
@@ -90,56 +116,25 @@ def run(num_tiles, resolution, random_seed, input_file, output_data_file, output
                 
                 logging.info(f"starting test {len(all_metrics)+1}/{total_tests}")
                 logging.info(f"compressor: {c['name']} level={c['level']} shuffle={c['shuffle']} quant={c['quant']}")
-                logging.info(f"loading {rslice}")
 
-                start = timer()
-                data = ds[rslice][resolution]["cells"][()]
-                end = timer()
-                read_dur = end - start
-                tile_metrics['read_time'] = read_dur
-                tile_metrics['nbytes'] = data.nbytes
-                tile_metrics['shape'] = data.shape
-                tile_metrics['read_bps'] = data.nbytes / read_dur
-                logging.info(f"loaded {data.shape} {data.nbytes} bytes, time {read_dur}s")
+                key = f"{rslice}/{resolution}/cells"
+                data = read_compress_write(ds, key, compressor, filters, output_data_file)
 
-
-                start = timer()
-                za = zarr.array(data, chunks=True, filters=filters, compressor=compressor)
-                end = timer()
-                compress_dur = end - start
-                tile_metrics['compress_time'] = compress_dur
-                tile_metrics['nbytes_written'] = za.nbytes_stored
-                tile_metrics['compress_bps'] = data.nbytes / compress_dur
-                tile_metrics['storage_ratio'] = data.nbytes / za.nbytes_stored
-                logging.info(f"compression time = {compress_dur}, bps = {data.nbytes / compress_dur}, ratio = {za.nbytes/za.nbytes_stored}")
-
-                start = timer()
-                zarr.save(output_data_file, za)
-                end = timer()
-                write_dur = end - start
-                tile_metrics['write_time'] = write_dur
-                tile_metrics['write_bps'] = za.nbytes_stored / write_dur
-                logging.info(f"write time = {write_dur}s")
-
-                total_read_time += read_dur
-                total_compress_time += compress_dur
-                total_write_time += write_dur
-
-                total_bytes_read += data.nbytes
-                total_bytes_stored += za.nbytes_stored
+                tile_metrics['read_time'] = data['read_time']
+                tile_metrics['bytes_read'] = data['bytes_read']
+                tile_metrics['shape'] = data['shape']
+                tile_metrics['read_bps'] = data['bytes_read'] / data['read_time']
+                tile_metrics['compress_time'] = data['compress_time']
+                tile_metrics['bytes_written'] = data['bytes_written']
+                tile_metrics['compress_bps'] = data['bytes_written'] / data['compress_time']
+                tile_metrics['storage_ratio'] = data['bytes_read'] / data['bytes_written']
+                tile_metrics['write_time'] = data['write_time']
+                tile_metrics['write_bps'] = data['bytes_written'] / data['write_time']
 
                 all_metrics.append(tile_metrics)
 
-
         df = pd.DataFrame.from_records(all_metrics)
         df.to_csv(output_metrics_file, index_label='test_number')
-            
-        print(f"compress bps {total_bytes_read / total_compress_time}")
-        print(f"compression ratio = {total_bytes_read/total_bytes_stored}")
-        print(f"read bps {total_bytes_read / total_read_time}")
-        print(f"write bps {total_bytes_stored / total_write_time}")
-        
-    
 
 if __name__ == "__main__": 
     main()
