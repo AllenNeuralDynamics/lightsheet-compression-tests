818534be6ad51894c9d0a18ef97775c73c6ff881
Cameron Arshadi <arshadic23@gmail.com>
2022-01-24 09:18:52 -0800

    plot_trunc_filter.py: code cleanup
    - prefer numpy functions where applicable

--- plot_trunc_filter.py
+++ plot_trunc_filter.py
@@ -16,22 +16,18 @@ maxy = 1598
 with tifffile.TiffFile(filepath) as f:
     z = zarr.open(f.aszarr(), 'r')
     # Extract ROI
-    data = z[slice][miny:maxy, minx:maxx]
-
+    data = z[slice, miny:maxy, minx:maxx]
+    trunc_bits = [0, 2, 4, 6, 8, 10]
     row = 2
     col = 3
     fig, ax = plt.subplots(row, col, figsize=(16, 16))
-    i = j = 0
-    for trunc_bits in [0, 2, 4, 6, 8, 10]:
-        filter = numcodecs.fixedscaleoffset.FixedScaleOffset(offset=0, scale=1.0 / (2 ** trunc_bits), dtype=np.uint16)
-        filtered = np.reshape(filter.decode(filter.encode(data)), data.shape)
+    for i, tb in enumerate(trunc_bits):
+        filt = numcodecs.fixedscaleoffset.FixedScaleOffset(offset=0, scale=1.0 / (2 ** tb), dtype=np.uint16)
+        filtered = np.reshape(filt.decode(filt.encode(data)), data.shape)
         metrics = compress_zarr.eval_quality(data, filtered, ['mse', 'ssim', 'psnr'])
-        ax[i, j].imshow(filtered, cmap='gray')
-        ax[i, j].set_title(f"trunc_bits={trunc_bits}, mse={round(metrics['mse'], 2)}, ssim={round(metrics['ssim'], 4)}")
-        j += 1
-        if j % col == 0:
-            j = 0
-            i += 1
+        idx = np.unravel_index(i, (row, col))
+        ax[idx].imshow(filtered, cmap='gray')
+        ax[idx].set_title(f"trunc_bits={tb}, mse={round(metrics['mse'], 2)}, ssim={round(metrics['ssim'], 4)}")
 
 fig.savefig('./trunc_filters.png', dpi=500)
 plt.show()
