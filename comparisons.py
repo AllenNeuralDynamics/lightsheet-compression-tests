d0836e36166e1da50a1dab2dbd50b942ee6ddf00
Cameron Arshadi <arshadic23@gmail.com>
2022-02-07 23:07:06 -0500

    minor updates

--- comparisons.py
+++ comparisons.py
@@ -10,19 +10,8 @@ def barplot(metrics_file, ax, x_key, y_key, secondary_y_key=None, bar_width=0.2,
         ax2 = ax.twinx()
     for i, compressor in enumerate(zarr_df['compressor_name'].unique().tolist()):
         df = zarr_df[zarr_df['compressor_name'] == compressor]
-        # txtbox = ""
-        if 'shuffle' in kwargs:
-            df = df[df['shuffle'] == kwargs['shuffle']]
-            # txtbox += f"shuffle={kwargs['shuffle']}\n"
-        if 'level' in kwargs:
-            df = df[df['level'] == kwargs['level']]
-            # txtbox += f"level={kwargs['level']}\n"
-        if 'trunc' in kwargs:
-            df = df[df['trunc'] == kwargs['trunc']]
-            # txtbox += f"trunc={kwargs['trunc']}\n"
-        if 'chunk_scale' in kwargs:
-            df = df[df['chunk_factor'] == kwargs['chunk_scale']]
-            # txtbox += f"chunk scale={kwargs['chunk_scale']}\n"
+        for key, val in kwargs.items():
+            df = df[df[key] == val]
         mean_df = df.groupby([x_key]).mean().sort_index()
         x = mean_df.index.to_numpy()
         y = mean_df[y_key].to_numpy()
@@ -33,7 +22,6 @@ def barplot(metrics_file, ax, x_key, y_key, secondary_y_key=None, bar_width=0.2,
             t = mean_df[secondary_y_key].to_numpy()
             ax2.plot(x, t)
             ax2.set_ylabel(secondary_y_key)
-    # ax.text(0, 0, txtbox, color='black', rotation=0, bbox=dict(facecolor=None, boxstyle="square,pad=0.3"))
     ax.set_ylabel(y_key)
     ax.set_xlabel(x_key)
 
@@ -41,7 +29,7 @@ def barplot(metrics_file, ax, x_key, y_key, secondary_y_key=None, bar_width=0.2,
 if __name__ == "__main__":
     x_keys = ['level', 'shuffle', 'trunc']
     y_keys = ['storage_ratio', 'compress_bps', 'cpu_utilization']
-    default_values = {
+    defaults = {
         'shuffle': 1,
         'trunc': 0,
         'level': 9
@@ -52,9 +40,9 @@ if __name__ == "__main__":
     import math
     rows = int(math.sqrt(n))
     cols = int(n / rows) + 1
-    fig, axes = plt.subplots(rows, cols, figsize=(9,13))
+    fig, axes = plt.subplots(rows, cols, figsize=(9, 13))
     for i, xy_keys in enumerate(keys):
-        defaults_no_x = default_values.copy()
+        defaults_no_x = defaults.copy()
         defaults_no_x.pop(xy_keys[0])  # remove varying parameter from defaults
         barplot(
             metrics_file=r"Y:\aindtemp\cameron.arshadi\compression-metrics_data.h5.csv",
